846e1307c14e9cb0bd5ef38853e64d9d8bc287c9
kxxoling <kxxoling@gmail.com>
2014-03-21 11:33:27 +0800

    using unittest rewrite test

--- test/orderlisttest.py
+++ test/orderlisttest.py
@@ -1,30 +1,45 @@
 # This Python file uses the following encoding: utf-8
+import unittest
 from random import randrange
 from orderlist import *
 
 TEST_TIMES = 10
+ARRAY_LENGTH = 10
 
 
 def create_random_list():
-    return [randrange(-10, 10) for i in range(10)]
+    return [randrange(-10, 10) for i in range(ARRAY_LENGTH)]
 
 
 def sort(unsorted_list):
     return unsorted_list.sort()
 
 
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.lists = [create_random_list() for i in range(TEST_TIMES)]
+
+    def test_bubble_sort(self):
+        for lis in self.lists:
+            li = lis
+            assert bubble_sort(li) == sorted(lis)
+
+    def test_insert_sort(self):
+        for lis in self.lists:
+            li = lis
+            assert insert_sort(li) == sorted(lis)
+
+    def test_select_sort(self):
+        for lis in self.lists:
+            li = lis
+            assert select_sort(li) == sorted(lis)
+
+    def test_quick_sort(self):
+        for lis in self.lists:
+            li = lis
+            quick_sort(li, 0, ARRAY_LENGTH-1)
+            assert li == sorted(lis)
+
+
 if __name__ == '__main__':
-    lis = [create_random_list() for i in range(TEST_TIMES)]
-    # for li in lis:
-    #     if bubble_sort(li) != sorted(li):
-    #         print '冒泡排序出错！原始列表：%s；排序后的列表：%s。' % (li, bubble_sort(li))
-    # for li in lis:
-    #     if insert_sort(li) != sorted(li):
-    #         print '插入排序出错！原始列表：%s；排序后的列表：%s。' % (li, insert_sort(li))
-    # for li in lis:
-    #     if select_sort(li) != sorted(li):
-    #         print '选择排序出错！原始列表：%s；排序后的列表：%s。' % (li, select_sort(li))
-    for li in lis:
-        if quick_sort(li,0,9) != sorted(li):
-            print '快速排序出错！原始列表：%s；排序后的列表：%s。' % (li, quick_sort(li,0,9))
-    print 'Finished!'
+    unittest.main()
